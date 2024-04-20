c65b0c8508fb70cc0d6a38764587fb772fc0f677
Ankush <akankush56086@gmail.com>
2024-04-20 13:41:46 +0530

    Added the timeline events and time

--- src/Components/Timeline/Timeline.jsx
+++ src/Components/Timeline/Timeline.jsx
@@ -12,9 +12,9 @@ function Timeline() {
                     <div className='timelineimgdiv'>
                     </div>
                     <div className="text-box lt">
-                        <h2><b>Event A</b></h2>
-                        <small>15th may 9:00 to 12:00</small>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sit praesentium ipsam aliquam voluptatum voluptate vero perferendis illum.</p>
+                        <h2><b>Inaugration</b></h2>
+                        <small>16th may 9:00 to 11:30</small>
+                        <p>Welcome to the dawn of innovation! Let the tech fest begin its electrifying journey.</p>
                         <span className='left-container-arrow'></span>
                     </div>
                 </div>
@@ -23,9 +23,9 @@ function Timeline() {
                     <div className='timelineimgdiv'>
                     </div>
                     <div className="text-box rt">
-                        <h2><b>Event B</b></h2>
-                        <small>15th may 9:00 to 12:00</small>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sit praesentium ipsam aliquam voluptatum voluptate vero perferendis illum.</p>
+                        <h2><b>Workshop</b></h2>
+                        <small>16th may 11:30 to 13:30</small>
+                        <p>At Anokhya, dive into creativity at our workshop. Learn, innovate, and be inspired by cutting-edge tech.</p>
                         <span className='right-container-arrow'></span>
                     </div>
                 </div>
@@ -34,9 +34,9 @@ function Timeline() {
                     <div className='timelineimgdiv'>
                     </div>
                     <div className="text-box lt">
-                        <h2><b>Event C</b></h2>
-                        <small>15th may 9:00 to 12:00</small>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sit praesentium ipsam aliquam voluptatum voluptate vero perferendis illum.</p>
+                        <h2><b>Meme Splash</b></h2>
+                        <small>16th may 11:30 to 13:30</small>
+                        <p>Dive into the laughter at Meme Splash! Join the fun-filled event and let the memes roll!</p>
                         <span className='left-container-arrow'></span>
                     </div>
                 </div>
@@ -46,33 +46,9 @@ function Timeline() {
                     <div className='timelineimgdiv'>
                     </div>
                     <div className="text-box rt">
-                        <h2><b>Event D</b></h2>
-                        <small>15th may 9:00 to 12:00</small>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sit praesentium ipsam aliquam voluptatum voluptate vero perferendis illum.</p>
-                        <span className='right-container-arrow'></span>
-                    </div>
-                </div>
-
-
-                <div className="maincontainer left-container">
-                    <div className='timelineimgdiv'>
-                    </div>
-                    <div className="text-box lt">
-                        <h2><b>Event E</b></h2>
-                        <small>15th may 9:00 to 12:00</small>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sit praesentium ipsam aliquam voluptatum voluptate vero perferendis illum.</p>
-                        <span className='left-container-arrow'></span>
-                    </div>
-                </div>
-
-
-                <div className="maincontainer right-container">
-                    <div className='timelineimgdiv'>
-                    </div>
-                    <div className="text-box rt">
-                        <h2><b>Event F</b></h2>
-                        <small>15th may 9:00 to 12:00</small>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sit praesentium ipsam aliquam voluptatum voluptate vero perferendis illum.</p>
+                        <h2><b>Code-Relay</b></h2>
+                        <small>16th may 14:30 to 16:30</small>
+                        <p>Race to the code at CodeRelay! Join the fast-paced event and conquer programming challenges together!</p>
                         <span className='right-container-arrow'></span>
                     </div>
                 </div>
@@ -82,20 +58,21 @@ function Timeline() {
                     <div className='timelineimgdiv'>
                     </div>
                     <div className="text-box lt">
-                        <h2><b>Event G</b></h2>
-                        <small>15th may 9:00 to 12:00</small>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sit praesentium ipsam aliquam voluptatum voluptate vero perferendis illum.</p>
+                        <h2><b>Hack-MY-PC</b></h2>
+                        <small>16th may 14:30 to 16:30</small>
+                        <p>Unlock innovation at Hack My PC! Join the immersive event and delve into the world of hacking.</p>
                         <span className='left-container-arrow'></span>
                     </div>
                 </div>
+
 
                 <div className="maincontainer right-container">
                     <div className='timelineimgdiv'>
                     </div>
                     <div className="text-box rt">
-                        <h2><b>Event H</b></h2>
-                        <small>15th may 9:00 to 12:00</small>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sit praesentium ipsam aliquam voluptatum voluptate vero perferendis illum.</p>
+                        <h2><b>Deploy It</b></h2>
+                        <small>16th may 16:30 to 18:00</small>
+                        <p>Dive into deployment at Deploy It! Join the event and explore the world of coding implementation and execution.</p>
                         <span className='right-container-arrow'></span>
                     </div>
                 </div>
@@ -105,9 +82,32 @@ function Timeline() {
                     <div className='timelineimgdiv'>
                     </div>
                     <div className="text-box lt">
-                        <h2><b>Event I</b></h2>
-                        <small>15th may 9:00 to 12:00</small>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sit praesentium ipsam aliquam voluptatum voluptate vero perferendis illum.</p>
+                        <h2><b>PC Building</b></h2>
+                        <small>16th may 16:30 to 18:00</small>
+                        <p>Participate in the excitement at PC Building! Dive into the event and craft your perfect machine from scratch.</p>
+                        <span className='left-container-arrow'></span>
+                    </div>
+                </div>
+
+                <div className="maincontainer right-container">
+                    <div className='timelineimgdiv'>
+                    </div>
+                    <div className="text-box rt">
+                        <h2><b>Tech Talk</b></h2>
+                        <small>17th may 9:00 to 10:00</small>
+                        <p>Engage with knowledge at Tech Talk! Immerse yourself in enlightening discussions and cutting-edge insights.</p>
+                        <span className='right-container-arrow'></span>
+                    </div>
+                </div>
+
+
+                <div className="maincontainer left-container">
+                    <div className='timelineimgdiv'>
+                    </div>
+                    <div className="text-box lt">
+                        <h2><b>UI/UX</b></h2>
+                        <small>17th may 11:00 to 13:00</small>
+                        <p>Dive into design at UI/UX! Explore the art of user interface and user experience creation.</p>
                         <span className='left-container-arrow'></span>
                     </div>
                 </div>
@@ -117,9 +117,9 @@ function Timeline() {
                     <div className='timelineimgdiv'>
                     </div>
                     <div className="text-box rt">
-                        <h2><b>Event J</b></h2>
-                        <small>15th may 9:00 to 12:00</small>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sit praesentium ipsam aliquam voluptatum voluptate vero perferendis illum.</p>
+                        <h2><b>Picture Probe</b></h2>
+                        <small>17th may 11:00 to 13:00</small>
+                        <p>Explore imagery at Picture Probe! Embark on a visual journey and delve into the world of photography.</p>
                         <span className='right-container-arrow'></span>
                     </div>
                 </div>
@@ -128,9 +128,9 @@ function Timeline() {
                     <div className='timelineimgdiv'>
                     </div>
                     <div className="text-box lt">
-                        <h2><b>Event K</b></h2>
-                        <small>15th may 9:00 to 12:00</small>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sit praesentium ipsam aliquam voluptatum voluptate vero perferendis illum.</p>
+                        <h2><b>Tech Tinder</b></h2>
+                        <small>17th may 14:00 to 16:00</small>
+                        <p>Discover connections at Tech Tinder! Engage in tech conversations and spark innovative collaborations.</p>
                         <span className='left-container-arrow'></span>
                     </div>
                 </div>
@@ -139,12 +139,23 @@ function Timeline() {
                     <div className='timelineimgdiv'>
                     </div>
                     <div className="text-box rt">
-                        <h2><b>Event L</b></h2>
-                        <small>15th may 9:00 to 12:00</small>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sit praesentium ipsam aliquam voluptatum voluptate vero perferendis illum.</p>
+                        <h2><b>Workshop</b></h2>
+                        <small>17th may 14:00 to 16:00</small>
+                        <p>Step into the world of innovation! Welcome to the workshop where learning meets creativity and discovery.</p>
                         <span className='right-container-arrow'></span>
                     </div>
 
+                </div>
+
+                <div className="maincontainer left-container">
+                    <div className='timelineimgdiv'>
+                    </div>
+                    <div className="text-box lt">
+                        <h2><b>Tech Tinder</b></h2>
+                        <small>17th may 14:00 to 16:00</small>
+                        <p>Discover connections at Tech Tinder! Engage in tech conversations and spark innovative collaborations.</p>
+                        <span className='left-container-arrow'></span>
+                    </div>
                 </div>
 
 
