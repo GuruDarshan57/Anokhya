0136b57eca8fa47173598abb6c0300b86d810c30
Gurudarshan57 <venom573116@gmail.com>
2024-04-27 13:24:37 +0530

    Fixing events

--- src/Components/Events/Events.jsx
+++ src/Components/Events/Events.jsx
@@ -21,7 +21,7 @@ const Events = () => {
                                 <div class="event-card-back" style={{ color: `${ele.colour_code}` }}>
                                     <h2>{ele.event_name}</h2>
                                     <p>{ele.event_description.length > 280 ? ele.event_description.slice(0, 280) + " ..... Read More" : ele.event_description}</p>
-                                    <button ><a href="/events/code_relay">Read More</a></button>
+                                    <a href="/events/code_relay">Explore More</a>
                                 </div>
                             </div>
                         </div>
@@ -32,7 +32,7 @@ const Events = () => {
             <div className="events_bottom">
                 <img src={bimg} alt="" />
             </div>
-        </div>
+        </div >
     )
 }
 
