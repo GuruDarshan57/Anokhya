2cde2228395ec395365a54d96fc66d8b926b050f
Gurudarshan57 <venom573116@gmail.com>
2024-05-10 17:15:44 +0530

    updated css

--- src/Components/Landing_Page/Landing_Page.jsx
+++ src/Components/Landing_Page/Landing_Page.jsx
@@ -5,25 +5,6 @@ import logo from '../../Assets/LPMV/Logo.png'
 import banner from '../../Assets/LPMV/Banner.png'
 
 function Home() {
-    // const [isOpen, setIsOpen] = useState(false);
-
-    // useEffect(() => {
-    //     const dropdown_menu = document.querySelector('.dropdown_menu');
-    //     const toggle_btnIcon = document.querySelector('.toggle_btn i');
-
-    //     const click = () => {
-    //         dropdown_menu.classList.toggle('open');
-    //         const isOpen = dropdown_menu.classList.contains('open');
-    //         toggle_btnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
-    //     };
-
-    //     const toggle_btn = document.querySelector('.toggle_btn');
-    //     toggle_btn.addEventListener('click', click);
-
-    //     return () => {
-    //         toggle_btn.removeEventListener('click', click);
-    //     };
-    // }, []);
 
     function handleButtonClick() {
         window.location.href = 'https://google.com/';
@@ -31,60 +12,6 @@ function Home() {
 
     return (
         <div className='body' id='Home'>
-            {/* <header className='header'>
-                <div className="navbar">
-                    <div className="logo">
-                        <a className="a" href="#">Anokhya</a>
-                    </div>
-                    <ul className="links">
-                        <li className='li'><a className='a' href="#Home">Home</a></li>
-                        <li className='li'><a className='a' href="#About">About</a></li>
-                        <li className='li'><a className='a' href="#Events">Events</a></li>
-                        <li className='li'><a className='a' href="#Timeline">Timeline</a></li>
-                        <li className='li'><a className='a' href="#Contact">Contact</a></li>
-                    </ul>
-
-
-
-                    <button className='dbroubutton' id='btnwin' onClick={handleButtonClick}>
-                        B R O C H U R E
-                        <div id="clip">
-                            <div id="leftTop" class="corner"></div>
-                            <div id="rightBottom" class="corner"></div>
-                            <div id="rightTop" class="corner"></div>
-                            <div id="leftBottom" class="corner"></div>
-                        </div>
-                        <span id="rightArrow" class="arrow"></span>
-                        <span id="leftArrow" class="arrow"></span>
-                    </button>
-
-                    <div className="toggle_btn">
-                        <i className="fa-solid fa-bars"></i>
-                    </div>
-                </div>
-
-                <div className='dropdown_menu'>
-                    <ul>
-                        <li className='li'><a className='a' href="#Home">Home</a></li>
-                        <li className='li'><a className='a' href="#About">About</a></li>
-                        <li className='li'><a className='a' href="#Events">Events</a></li>
-                        <li className='li'><a className='a' href="#Timeline">Timeline</a></li>
-                        <li className='li'><a className='a' href="#Contact">Contact</a></li>
-                        <li className='li'><button className='dbroubutton'>
-                            B R O C H U R E
-                            <div id="clip">
-                                <div id="leftTop" class="corner"></div>
-                                <div id="rightBottom" class="corner"></div>
-                                <div id="rightTop" class="corner"></div>
-                                <div id="leftBottom" class="corner"></div>
-                            </div>
-                            <span id="rightArrow" class="arrow"></span>
-                            <span id="leftArrow" class="arrow"></span>
-                        </button>
-                        </li>
-                    </ul>
-                </div>
-            </header > */}
             <div div className="home_m" >
                 <div className="home_m_top_left">
                     <h3>TECH FEST 2024</h3>
